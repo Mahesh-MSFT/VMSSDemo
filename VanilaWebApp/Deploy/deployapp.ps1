0e4f5b6ab70c2b1fd3d30aaa0c2ba38f7fca1f3f
Mahesh Kshirsagar <maksh@microsoft.com>
2018-02-14 12:39:59 +0530

    Correccted .Net Core Versioning

--- VanilaWebApp/Deploy/deployapp.ps1
+++ VanilaWebApp/Deploy/deployapp.ps1
@@ -69,10 +69,10 @@ Invoke-Expression -Command $Command
 Write-Output "IIS Reset completed ..."
 
 # Install the .NET Core SDK
-Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848827 -outfile $env:temp\dotnet-dev-win-x64.1.0.4.exe
-Start-Process $env:temp\dotnet-dev-win-x64.1.0.4.exe -ArgumentList '/quiet' -Wait
+#Invoke-WebRequest https://go.microsoft.com/fwlink/?linkid=848827 -outfile $env:temp\dotnet-dev-win-x64.1.0.4.exe
+#Start-Process $env:temp\dotnet-dev-win-x64.1.0.4.exe -ArgumentList '/quiet' -Wait
 
-# Install the .NET Core Windows Server Hosting bundle
+# Install the .NET Core 2.0 Windows Server Hosting bundle
 #Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile $env:temp\DotNetCore.WindowsHosting.exe
 Invoke-WebRequest https://aka.ms/dotnetcore-2-windowshosting -outfile $env:temp\DotNetCore.2.0.5-WindowsHosting.exe
 
