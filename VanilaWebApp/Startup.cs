1e822ded50d8e59c1dcff8c43711265351fba5f0
Mahesh Kshirsagar <maksh@microsoft.com>
2018-02-14 10:24:22 +0530

    Added IIS Integration

--- VanilaWebApp/Startup.cs
+++ VanilaWebApp/Startup.cs
@@ -21,6 +21,9 @@ namespace VanilaWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<IISOptions>(options =>
+            {
+            });
             services.AddMvc();
         }
 
